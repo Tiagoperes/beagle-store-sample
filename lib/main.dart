af82065c527acec0ad3c5c0e8f5bb4797fff2b14
james <jamesson.penha@zup.com.br>
2022-02-10 15:23:40 -0300

    Add product details screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,26 @@
-
 import 'package:beagle/beagle.dart';
+import 'package:beagle_components/beagle_components.dart';
 import 'package:flutter/material.dart';
 import 'package:sample/beagle.dart';
+import 'package:sample/theme/index.dart';
 
 import 'main-navigator.dart';
 
 void main() {
   runApp(BeagleProvider(
-    beagle: beagleService,
-    child: MaterialApp(
-      title: 'Beagle Sample',
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        indicatorColor: Colors.white,
-        appBarTheme: AppBarTheme(
-          elevation: 0,
+      beagle: beagleService,
+      child: BeagleThemeProvider(
+        theme: CustomTheme(),
+        child: MaterialApp(
+          title: 'Beagle Sample',
+          theme: ThemeData(
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            indicatorColor: Colors.white,
+            appBarTheme: AppBarTheme(
+              elevation: 0,
+            ),
+          ),
+          home: MainNavigator(),
         ),
-      ),
-      home: MainNavigator(),
-    ),
-  ));
+      )));
 }
