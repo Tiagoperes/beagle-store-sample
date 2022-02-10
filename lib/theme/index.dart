0bc8cfee06a878b622c6bb4fdd90c07c1e631aa7
james <jamesson.penha@zup.com.br>
2022-02-10 15:52:39 -0300

    Add product details screen

--- lib/theme/index.dart
+++ lib/theme/index.dart
@@ -1,6 +1,5 @@
 import 'package:beagle_components/beagle_components.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/material/button_style.dart';
 import 'package:flutter/widgets.dart';
 
 class CustomTheme extends BeagleTheme {
