67257ad9ec7109735a56a1de0c7b9e9d81adb9c9
Tiago Peres França <tiago.franca@zup.com.br>
2022-02-18 17:06:47 -0300

    removes zscaler

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:beagle/beagle.dart';
 import 'package:beagle_components/beagle_components.dart';
 import 'package:flutter/material.dart';
 import 'package:sample/beagle.dart';
-import 'package:sample/security.dart';
 import 'package:sample/theme.dart';
 
 import 'main-navigator.dart';
 
 void main() async {
-  await trustZscaler();
   runApp(BeagleProvider(
       beagle: beagleService,
       child: BeagleThemeProvider(
